0bffd35b45a54646eada84d9616365ea053b617e
Ruby Hoseinzadeh <robina.sre78@gmail.com>
2022-02-08 00:21:17 -0800

    map

--- src/MapRestaurants/MapRestaurant.js
+++ src/MapRestaurants/MapRestaurant.js
@@ -1,6 +1,5 @@
 import React, {useEffect, useState} from 'react';
 import ReactMapGl, {Marker, Popup} from "react-map-gl";
-import mapboxgl from '!mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import {Fastfood } from '@mui/icons-material';
 import axios from "axios";
